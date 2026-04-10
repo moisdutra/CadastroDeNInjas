49266469aaeb82a4653defeff8ab0b09ea57b978
moisdutra <moisesbrdg3@hotmail.com>
2026-04-10 19:47:14 -0300

    feat: Rota de findById adicionada

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -32,9 +32,9 @@ public class NinjaController {
     }
 
     // Mostrar ninja por ID
-    @GetMapping("/listarID")
-    public String mostrarTodosOsNinjasPorID() {
-        return "Mostrar Ninja por ID";
+    @GetMapping("/listar/{id}")
+    public NinjaModel listarNinjasPorID(@PathVariable Long id) {
+        return ninjaService.listarNinjasPorID(id);
     }
 
     // Alterar dados dos ninjas
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -3,6 +3,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NinjaService {
@@ -17,6 +18,11 @@ public class NinjaService {
     public List<NinjaModel> listarNinjas() {
         return ninjaRepository.findAll();
     }
-    
+
+    // Listar os Ninjas por ID
+    public NinjaModel listarNinjasPorID(Long id) {
+        Optional<NinjaModel> ninjaPorID = ninjaRepository.findById(id);
+        return ninjaPorID.orElse(null);
+    }
 
 }
