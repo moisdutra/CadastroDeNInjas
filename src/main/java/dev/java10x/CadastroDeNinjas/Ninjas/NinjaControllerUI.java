bc670303b8e2e989c5fd6a259bada54276816a9c
moisdutra <moisesbrdg3@hotmail.com>
2026-04-14 18:31:09 -0300

    feat: Implementa UI para tabela de cadastro de Ninjas, cria a controller com as rotas de UI para cadastro, deleção e listagem de ninjas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
@@ -0,0 +1,57 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
+public class NinjaControllerUI {
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUI(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/listar")
+    public String listarNinjas(Model model) {
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        model.addAttribute("ninjas", ninjas);
+        return "listarNinjas";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarNinjasPorID(@PathVariable Long id) {
+        ninjaService.deletarNinjaPorID(id);
+        return "redirect:/ninjas/ui/listar";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String listarNinjasPorID(@PathVariable Long id, Model model) {
+        NinjaDTO ninja = ninjaService.listarNinjasPorID(id);
+        if (ninja != null) {
+            model.addAttribute("ninja", ninja);
+            return "detalhesNinja";
+        } else {
+            model.addAttribute("mensagem", "Ninja não encontrado");
+            return "listarNinjas";
+        }
+    }
+
+    @GetMapping("/adicionar")
+    public String mostrarFormularioAdicionarNinja(Model model) {
+        model.addAttribute("ninja", new NinjaDTO());
+        return "adicionarNinja";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarNinja(@ModelAttribute NinjaDTO ninja, RedirectAttributes redirectAttributes) {
+        ninjaService.criarNinja(ninja);
+        redirectAttributes.addAttribute("mensagem", "Ninja salvo com sucesso");
+        return "redirect:/ninjas/ui/listar";
+    }
+
+}
